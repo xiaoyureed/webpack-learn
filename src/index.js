0bccd93063b1b1f899adda7457025b57e57fad97
xiaoyu <775000738@qq.com>
2018-06-22 00:32:15 +0800

    清理

--- src/index.js
+++ src/index.js
@@ -1,22 +1,11 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
 
 function component() {
     var element = document.createElement('div');
   
     // Lodash, js工具库
     element.innerHTML = _.join(['你好', 'webpack'], ' ');
-    element.classList.add('hello');
-
-    // 添加图片, 还可以在css中使用 background: url('./icon.png')
-    const icon = new Image();
-    icon.src = Icon;
-    element.appendChild(icon);
-
-    console.log(Data);
-  
+    
     return element;
   }
   
