e1fc482b764444a84ed8b4dedbd086e9bf8a50b1
xiaoyu <775000738@qq.com>
2020-04-01 07:47:24 +0800

    --

---
+++ src/hmr/index.js
@@ -0,0 +1,40 @@
+/**
+ * hmr(模块热替换) demo
+ */
+import _ from 'lodash';
+import printSth from './print.js';
+import './styles.css';
+
+function component() {
+    var element = document.createElement('div');
+    // Lodash, js工具库
+    element.innerHTML = _.join(['你好', 'webpack'], ' ');
+    
+    var btn = document.createElement('button');
+    btn.innerHTML = '点击, 然后到console查看';
+    btn.onclick = printSth;// 加上括号会自动执行， 不点击就执行了
+
+    element.appendChild(btn);
+    
+    return element;
+}
+  
+const exec = () => {
+  // document.body.appendChild(component());
+  let element = component(); // 给下面的 hmr代码用
+  document.body.appendChild(element);
+
+  // HMR代码
+  if (module.hot) {
+    // accept('moduleId', callback), 这里仅仅针对print.js热替换, 如果是accept(), 全体热替换
+    module.hot.accept('./print.js', function() {
+      console.log('探测到 printSth 的更新.');
+      // printSth();
+      document.body.removeChild(element);
+      element = component();//点击时, 重新渲染 component
+      document.body.appendChild(element);
+    });
+  }
+};
+
+export default exec;
--- webpack.config.js
+++ webpack.config.js
@@ -4,22 +4,30 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const webpack = require('webpack');// 使用webpack 内建的插件, 实现 HMR功能(hmr: 无刷新更新页面), 如果使用的是webpack-dev-middleware instead of webpack-dev-server, 则无法使用内建插件实现 hmr, 需要 webpack-hot-middleware插件
 
 module.exports = {
-    // entry: './src/index.js',
+    // entry: './src/index.js', // 等同于  main: './path/to/my/entry/file.js'
+    // 多页面
+    // entry: {
+    //     pageOne: './src/pageOne/index.js',
+    //     pageTwo: './src/pageTwo/index.js',
+    //     pageThree: './src/pageThree/index.js'
+    // },
     entry: {
         app: './src/index.js'
     },
     output: {
         // filename: 'main.js',
-        // [name]表示entry中的key
-        filename: '[name].bundle.js',
+        // [name]表示entry中的key, app.bundle.js
+        filename: '[name].bundle.js', // 最少必须, 默认输出到 'dist' 文件夹
         path: path.resolve(__dirname, 'dist'),
         publicPath: '/' // 表示 express将会启动在 http://localhost:3000, 在 server.js中指定
     },
+    // mode: 'production', // none, development or production(default). 或者在cli中 'webpack --mode=production'
     module: {
         rules: [
             {
                 test: /\.css$/,
                 use: [// 更多 loader: https://webpack.js.org/guides/hot-module-replacement/#other-code-and-frameworks
+                    // 一般来说需要引入css-loader和style-loader，其中css-loader用于解析，而style-loader则将解析后的样式嵌入js代码。
                     'style-loader', 'css-loader'
                 ]
             }
@@ -28,9 +36,10 @@ module.exports = {
     plugins: [
         new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
-            title: 'output manage'// 指定index.html的title
+            title: 'output manage',// 指定index.html的title
+            // template: './src/index.html', // 指定一个index.html模板, 会原样赋值到dist
         }),
-        new webpack.NamedModulesPlugin(),// 方便的查看module的依赖关系, HMR用
+        new webpack.NamedModulesPlugin(),// 方便的查看module的依赖关系, HMR用; // development 会将 process.env.NODE_ENV 的值设为 development 。启用 NamedChunksPlugin 和 NamedModulesPlugin。
         new webpack.HotModuleReplacementPlugin() // 支持hmr
     ],
     // mode: "development", // 设定为开发模式(代码不会压缩), 还有 production模式(压缩代码), 命令行指定: --mode xxx
